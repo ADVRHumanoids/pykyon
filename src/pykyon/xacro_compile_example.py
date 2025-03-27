a881089cd07ab2c22dd52060446c27d5aa97f683
Carlo Rizzardo <cr.git.email@gmail.com>
2025-03-27 15:30:25 +0100

    rearranged package

--- src/pykyon/xacro_compile_example.py
+++ src/pykyon/xacro_compile_example.py
@@ -1,8 +1,8 @@
 from adarl.utils.utils import compile_xacro_string, pkgutil_get_path
 from pathlib import Path
 
-model_path = pkgutil_get_path("iit-kyon-ros-pkg","kyon_urdf/urdf/kyon.urdf.xacro")
+model_path = pkgutil_get_path("pykyon", "iit-kyon-ros-pkg/kyon_urdf/urdf/kyon.urdf.xacro")
 urdf_string = compile_xacro_string( model_definition_string=Path(model_path).read_text(),
                                     model_kwargs={},
-                                    extra_pkg_paths={   "kyon_urdf" : pkgutil_get_path("iit-kyon-ros-pkg","kyon_urdf")})
+                                    extra_pkg_paths={   "kyon_urdf" : pkgutil_get_path("pykyon", "iit-kyon-ros-pkg/kyon_urdf")})
 print(urdf_string)
